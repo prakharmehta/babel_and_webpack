dbc6bcf1054caefcff4602d18270257331bc7262
Prakhar Mehta <prakh.mehta@gmail.com>
2022-01-31 01:29:20 +0530

    added webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
+
 module.exports = {
-    entry:"./src/index.js",
-    output: {
-        path: ""
-    }
-}
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/assets"),
+    filename: "bundle.js",
+  },
+};
