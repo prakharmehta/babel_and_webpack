b15a0eb57b4dc68c9269ef5fd536f094d27ab262
Prakhar Mehta <prakh.mehta@gmail.com>
2022-02-01 13:38:52 +0530

    webpack boilerplate

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import { styleBody, addTitle, contact } from "./dom";
-//for default exports, {} are not required, even the name of the import can be different
-import users, { getPremiumUsers } from "./data";
-
-const premUsers = getPremiumUsers(users);
-styleBody();
-addTitle("hello");
-console.log(users);
-console.log(premUsers);
